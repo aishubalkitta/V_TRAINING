843cb4099019ab44d008e7cbe03f9ca7cb682702
aishubalkitta <113792644+aishubalkitta@users.noreply.github.com>
2022-11-29 20:44:11 +0530

    Delete demoproject directory

--- demoproject/src/main/java/com/example/demoproject/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demoproject.repository;  
-import org.springframework.data.jpa.repository.JpaRepository;  
-import com.example.demoproject.model.Student;  
-public interface StudentRepository extends JpaRepository<Student, Integer>  
-{  
-	
-}
